07cc45ed1de20b5df9e67563065154112a7e1c0d
dmelio <d.melio.escobar@gmail.com>
2022-09-01 22:33:37 -0400

    PortReact
    Portafolio con React y asp.net Core

---
+++ PortReact/PortReact/ClientApp/src/componentes/Inicio.jsx
@@ -0,0 +1,19 @@
+﻿const Inicio = () => {
+    return (
+        <div className="Container-title">
+            <div className="row">
+                <h5>Hola, soy</h5>
+                <h2>Daniel Melio Escobar</h2>
+                <h5>Analista programador</h5>
+                <div className="rowbtncv">
+                    <a href="doc/Daniel Melio Escobar.pdf"><button type="button" className="btn btn-primary" id="btncv">Descargar CV</button></a>
+                </div>
+                <div class="rowFotoP">
+                    <img class="img1port" alt="daniel melio foto perfil 1"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Inicio;
---
+++ PortReact/PortReact/ClientApp/src/componentes/Modals.jsx
@@ -0,0 +1,27 @@
+﻿import { useModal } from "../hooks/useModal";
+import Modal from "./Modal";
+
+const Modals = () => {
+    const [isOpenModal1,openModal1,closeModal1] = useModal(false)
+    return (
+        <div><h2>Modales</h2>
+            <button onClick={openModal1 }>Contacto</button>
+            <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
+                <form id="form">
+                    <h2 className="modal_title">Contactame</h2>
+                    <div className="mb-3">
+                        <label className="form-label">Correo</label>
+                        <input type="email" className="form-control" placeholder="correo" name="email" required />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Mensaje</label>
+                        <textarea type="text" className="form-control" placeholder="Mensaje" name="message" required></textarea>
+                    </div>
+                    <input type="submit" className="btnsend" id="button" value="Enviar" />
+                </form>
+            </Modal>
+        </div>
+     )
+}
+
+export default Modals;
