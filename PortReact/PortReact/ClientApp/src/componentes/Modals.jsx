bbe9b1f26f10a488e512ce3b48b72cb03c0e5aed
dmelio <d.melio.escobar@gmail.com>
2022-09-05 11:16:50 -0300

    Act
    Se agrega función para enviar correos

--- PortReact/PortReact/ClientApp/src/componentes/Modals.jsx
+++ PortReact/PortReact/ClientApp/src/componentes/Modals.jsx
@@ -1,8 +1,16 @@
 ﻿import { useModal } from "../hooks/useModal";
+import emailjs from "emailjs-com";
 import Modal from "./Modal";
 
 const Modals = () => {
-    const [isOpenModal1,openModal1,closeModal1] = useModal(false)
+    const [isOpenModal1, openModal1, closeModal1] = useModal(false)
+    function enviarEmail(e) {
+        e.preventDefault();
+        emailjs.sendForm('service_fks96pm', 'template_0a11r99', e.target,'sYSNjIdQkdwDqcmoV').then(res => {
+            alert("Se ha enviado correctamente.");
+            console.log(res);
+        })
+    }
     return (
         <header>
             <nav id="menu">
@@ -13,7 +21,8 @@ const Modals = () => {
                     </li>
                     <li type="circle"><a onClick={openModal1} href="#"><img className="imgnavmail"
                         width="40px"
-                        height="40px" alt=""/></a>
+                        height="40px" alt="" /></a>
+                        
                     </li>
                     <li type="circle"><a href="https://wa.me/56961772339"><img className="imgnavwsp"
                         width="40px"
@@ -25,8 +34,8 @@ const Modals = () => {
                     </li>   
                 </ul>
             </nav>
-            <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
-                <form id="form">
+            <Modal isOpen={isOpenModal1} closeModal={closeModal1} enviarEmail={enviarEmail}>
+                <form id="form" onSubmit={enviarEmail}>
                     <h2 className="modal_title">Contactame</h2>
                     <div className="mb-3">
                         <label className="form-label">Correo</label>
